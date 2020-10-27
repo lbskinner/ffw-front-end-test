365f09a43af609ecd822f968b36fda12f2cbf5a6
Leila Skinner <beileiwang@gmail.com>
2020-10-27 17:54:35 -0500

    add style to font cards

--- src/components/MyFonts/MyFonts.js
+++ src/components/MyFonts/MyFonts.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 20px;
+`;
+
+const ColorBlock = styled.div`
+  width: 88px;
+  height: 88px;
+  border: 1px solid black;
+  border-radius: 15px;
+  background-color: ${(props) => props.colorProps};
+  padding: 5px;
+  background-clip: content-box;
+  box-shadow: inset 0 0 0 5px #fff;
+  position: relative;
+`;
+
+const ColoredLetter = styled.span`
+  position: absolute;
+  bottom: 10px;
+  left: 16px;
+  font-size: 2rem;
+  font-weight: 900;
+  color: #fff;
+  opacity: 0.3;
+`;
 
 function MyFonts() {
-  return <div>My fonts tab</div>;
+  const [myFontsData, setMyFontsData] = useState([]);
+
+  const fetchMyFontsData = async () => {
+    try {
+      const url = "http://json.ffwagency.md/fonts_a";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.content);
+      setMyFontsData([...data.content]);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMyFontsData();
+  }, []);
+
+  return (
+    <Wrapper>
+      {myFontsData.map((font) => {
+        return (
+          <ul key={font.id}>
+            <ColorBlock colorProps={font.color}>
+              <ColoredLetter>{font.abbr}</ColoredLetter>
+            </ColorBlock>
+            <li>{font.label}</li>
+          </ul>
+        );
+      })}
+    </Wrapper>
+  );
 }
 
 export default MyFonts;
