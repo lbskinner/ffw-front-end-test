b8a53a4c9db5fd8ed62efb1b3c69d56115131970
Leila Skinner <beileiwang@gmail.com>
2020-10-27 17:05:42 -0500

    clicking tab changes contents displayed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import BuyFonts from "../BuyFonts/BuyFonts";
 import MyFonts from "../MyFonts/MyFonts";
 import "./App.css";
 
 function App() {
+  const [displayMyFonts, setDisplayMyFonts] = useState(true);
+
+  const handleClickMyFontsTab = () => {
+    setDisplayMyFonts(true);
+  };
+
+  const handleClickBuyFontsTab = () => {
+    setDisplayMyFonts(false);
+  };
+
   return (
     <div className="AppContainer">
       <div className="textContainer">
         <h2>Please select one font</h2>
         <div className="tabContainer">
-          <h6>MY FONTS</h6>
-          <h6>BUY FONTS</h6>
+          <h6 onClick={handleClickMyFontsTab}>MY FONTS</h6>
+          <h6 onClick={handleClickBuyFontsTab}>BUY FONTS</h6>
         </div>
       </div>
       <div className="fontsContainer">
-        {/* <MyFonts /> */}
-        <BuyFonts />
+        {displayMyFonts ? <MyFonts /> : <BuyFonts />}
       </div>
     </div>
   );
