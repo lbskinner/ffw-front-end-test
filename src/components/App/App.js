3129682798290dad3a11ea7d1e085aaa41322c9a
Leila Skinner <beileiwang@gmail.com>
2020-10-27 15:44:37 -0500

    added basic styles to app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        Please select one font
+    <div className="AppContainer">
+      <div className="textContainer">
+        <h2>Please select one font</h2>
+        <div className="tabContainer">
+          <h6>MY FONTS</h6>
+          <h6>BUY FONTS</h6>
+        </div>
+      </div>
+      <div className="fontsContainer">
         <MyFonts />
         <BuyFonts />
       </div>
