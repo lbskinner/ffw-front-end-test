8fa2618a1db4b116acf659280df348d35ae2e354
Leila Skinner <beileiwang@gmail.com>
2020-10-28 14:28:09 -0500

    created api component to get data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import { fetchData } from "../../API";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import BuyFonts from "../BuyFonts/BuyFonts";
 import MyFonts from "../MyFonts/MyFonts";
+
 import "./App.css";
 
-function App({ dispatch }) {
+function App({ store, dispatch }) {
   const [displayMyFonts, setDisplayMyFonts] = useState(true);
   const [tabsData, setTabsData] = useState([]);
 
   const fetchTabsData = async () => {
     try {
-      const url = "http://json.ffwagency.md/tabs";
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      setTabsData([...data]);
+      const tabData = await fetchData("tabs");
+      setTabsData([...tabData]);
       dispatch({
         type: "SET_TAB_DATA",
-        payload: [...data],
+        payload: [...tabData],
       });
     } catch (error) {
       console.warn(error);
@@ -38,7 +37,7 @@ function App({ dispatch }) {
       <div className="textContainer">
         <h2>Please select one font</h2>
         <div className="tabContainer">
-          {tabsData.map((tab) => {
+          {store.tabsReducer.map((tab) => {
             return (
               <h6 key={tab.id} onClick={() => handleClickMyFontsTab(tab.label)}>
                 {tab.label.toUpperCase()}
